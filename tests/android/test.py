f493e4fcba319f892f09295e973452bb02ebb4bd
ejmenkhorst <ejmenkhorst@gmail.com>
2023-08-24 12:53:58 +0200

    Local project setup for windows

--- tests/android/test.py
+++ tests/android/test.py
@@ -5,7 +5,8 @@ def test_android_click(appium_service, android_driver_factory):
     # Usage of the context manager ensures the driver session is closed properly
     # after the test completes. Otherwise, make sure to call `driver.quit()` on teardown.
     with android_driver_factory({
-        'appium:app': '/Users/erikmenkhorst/Documents/GitHub/appium2-python-portfolio/testapps/proverbial_android.apk',
+        # 'appium:app': '/Users/erikmenkhorst/Documents/GitHub/appium2-python-portfolio/testapps/proverbial_android.apk',
+        'appium:app': 'C:\\Users\\emenkhorst\\Desktop\\Local repo\\appium2-python-portfolio\\testapps\\proverbial_android.apk',
         'appium:udid': 'emulator-5554',
     }) as driver:
         el = driver.find_element(by=AppiumBy.ID, value='com.lambdatest.proverbial:id/Textbox')
