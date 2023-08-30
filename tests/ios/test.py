69701429dce4fb9ca5d026e0e1f9467fe6e96cd8
Erik  Menkhorst <ejmenkhorst@gmail.com>
2023-08-30 17:16:37 +0200

    Added Black and PyLint to run locally in pre-commit hook and in GitHub Actions.

--- tests/ios/test.py
+++ tests/ios/test.py
@@ -1,11 +1,15 @@
+"""driver to use locators."""
 from appium.webdriver.common.appiumby import AppiumBy
 
 
-def test_ios_click(appium_service, ios_driver_factory):
-	# Usage of the context manager ensures the driver session is closed properly
-	# after the test completes. Otherwise, make sure to call `driver.quit()` on teardown.
-	with ios_driver_factory({
-		'appium:app': 'testapps/proverbial_ios.ipa'  # /path/to/app/UICatalog.app.zip
-	}) as driver:
-		el = driver.find_element(by=AppiumBy.ID, value='com.lambdatest.proverbial:id/Textbox')
-		assert el.text == "Hello! Welcome to lambdatest Sample App called Proverbial"
+def test_ios_click(ios_driver_factory):
+    """Test if the mobile app can find the text on IOS app"""
+    # Usage of the context manager ensures the driver session is closed properly
+    # after the test completes. Otherwise, make sure to call `driver.quit()` on teardown.
+    with ios_driver_factory(
+        {"appium:app": "testapps/proverbial_ios.ipa"}  # /path/to/app/UICatalog.app.zip
+    ) as driver:
+        element = driver.find_element(
+            by=AppiumBy.ID, value="com.lambdatest.proverbial:id/Textbox"
+        )
+        assert element.text == "Hello! Welcome to lambdatest Sample App called"
