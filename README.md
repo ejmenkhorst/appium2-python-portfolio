1dc7ba303cf22e526705f5c2223230d5435ef44a
Erik <ejmenkhorst@gmail.com>
2023-08-30 17:29:03 +0200

    Integrate Black and PyLint in GitHub Actions. (#2)
    * Get all project dependencies via requirements.txt
    * Added Black and PyLint to run locally in pre-commit hook and in GitHub Actions.

--- README.md
+++ README.md
@@ -10,7 +10,8 @@ repository.
 
 ## Installation instructions for local setup
 
-Please follow the [installation instructions mac](./documentation/installation_mac.md) before executing tests in this
+Please follow the [installation instructions mac](./documentation/installation_mac.md) before executing tests in
+this[requirements.txt](requirements.txt)
 framework.
 
 ## Local project setup  
@@ -24,3 +25,33 @@ In order to run tests locally we need to do the following:
 1. Start appium server via terminal with ``` appium ```
 2. Start emulator you want to run the tests on
 3. Run the tests ``` pytest tests/android/test.py ```
+
+## Code formatting and linting
+
+For checking for best practices we use PyLint and for automatically setting up the correct formatting of code we
+use [Black](https://black.readthedocs.io/en/stable/index.html)
+
+### PyLint
+
+PyLint checks for best practices and other code smells.  
+The configuration of the PyLint is done in [pyproject.toml](./pyproject.toml).
+
+### Black
+
+The configuration of the Black formatter is done in [pyproject.toml](./pyproject.toml).  
+Via the command: ⌥⌘L in PyCharm you can configure when to run Black -> I prefer this on save and reformat file.
+
+### Git pre-commit-hook
+
+Via the plugin pre-commit defined in requirements.txt the Black plugin wil automatically run on pre-commit stage.  
+This is also defined in the file [.git/hooks/pre-commit](.git/hooks/pre-commit)
+
+```
+#!/bin/sh
+
+#Check file formatting and give a warning in the terminal
+black --check ./tests/
+#Check best practices via PyLint
+pylint $(git ls-files '*.py') 
+
+```
--- tests/conftest.py
+++ tests/conftest.py
@@ -1,3 +1,4 @@
+"""Module for running PyTest framework."""
 import pytest
 
 from appium import webdriver
@@ -6,63 +7,70 @@ from appium.options.ios import XCUITestOptions
 from appium.webdriver.appium_service import AppiumService
 
 APPIUM_PORT = 4723
-APPIUM_HOST = '127.0.0.1'
+APPIUM_HOST = "127.0.0.1"
 
 
-@pytest.fixture(scope='session')
+@pytest.fixture(scope="session")
 def appium_service():
-	service = AppiumService()
-	service.start(
-		# Check the output of `appium server --help` for the complete list of
-		# server command line arguments
-		args=['--address', APPIUM_HOST, '-p', str(APPIUM_PORT)],
-		timeout_ms=20000,
-	)
-	yield service
-	service.stop()
+    """Starting Appium Service before running tests."""
+    service = AppiumService()
+    service.start(
+        # Check the output of `appium server --help` for the complete list of
+        # server command line arguments
+        args=["--address", APPIUM_HOST, "-p", str(APPIUM_PORT)],
+        timeout_ms=20000,
+    )
+    yield service
+    service.stop()
 
 
 def create_ios_driver(custom_opts=None):
-	options = XCUITestOptions()
-	options.platformVersion = '13.4'
-	options.udid = '123456789ABC'
-	if custom_opts is not None:
-		options.load_capabilities(custom_opts)
-	# Appium1 points to http://127.0.0.1:4723/wd/hub by default
-	return webdriver.Remote(f'http://{APPIUM_HOST}:{APPIUM_PORT}', options=options)
+    """Create IOS driver."""
+    options = XCUITestOptions()
+    options.platformVersion = "13.4"
+    options.udid = "123456789ABC"
+    if custom_opts is not None:
+        options.load_capabilities(custom_opts)
+    # Appium1 points to http://127.0.0.1:4723/wd/hub by default
+    return webdriver.Remote(f"http://{APPIUM_HOST}:{APPIUM_PORT}", options=options)
 
 
 def create_android_driver(custom_opts=None):
-	options = UiAutomator2Options()
-	options.platformVersion = '10'
-	options.udid = 'emulator-5554'
-	if custom_opts is not None:
-		options.load_capabilities(custom_opts)
-	# Appium1 points to http://127.0.0.1:4723/wd/hub by default
-	return webdriver.Remote(f'http://{APPIUM_HOST}:{APPIUM_PORT}', options=options)
+    """Create Android driver."""
+    options = UiAutomator2Options()
+    options.platformVersion = "10"
+    options.udid = "emulator-5554"
+    if custom_opts is not None:
+        options.load_capabilities(custom_opts)
+    # Appium1 points to http://127.0.0.1:4723/wd/hub by default
+    return webdriver.Remote(f"http://{APPIUM_HOST}:{APPIUM_PORT}", options=options)
 
 
 @pytest.fixture
 def ios_driver_factory():
-	return create_ios_driver
+    """ios_driver_factory"""
+    return create_ios_driver
 
 
 @pytest.fixture
 def ios_driver():
-	# prefer this fixture if there is no need to customize driver options in tests
-	driver = create_ios_driver()
-	yield driver
-	driver.quit()
+    """return ios_driver to the function using it, quit driver afterwards."""
+    # prefer this fixture if there is no need to customize driver options in tests
+    driver = create_ios_driver()
+    yield driver
+    driver.quit()
 
 
 @pytest.fixture
 def android_driver_factory():
-	return create_android_driver
+    """android_driver_factory"""
+    return create_android_driver
 
 
 @pytest.fixture
 def android_driver():
-	# prefer this fixture if there is no need to customize driver options in tests
-	driver = create_android_driver()
-	yield driver
-	driver.quit()
+    """return android_driver to the function using it, quit driver afterwards."""
+    # prefer this fixture if there is no need to customize driver options in tests
+    driver = create_android_driver()
+    yield driver
+    driver.quit()
